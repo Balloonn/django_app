04f8b6fc61d23b3eb73c5c7f14d6133863480502
Balloonn <wangzhongnan@emails.bjut.edu.cn>
2022-08-22 18:48:54 +0800

    add first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">my webpage</h1>'
+    line2 = '<a href="/play/">进入游戏</a>'
+    return HttpResponse(line1 + line2)
+
+def play(request):
+    line1 = '<h1 style="text-align: center">游戏界面</h1>'
+    line2 = '<a href="/">返回主页面</a>'
+    return HttpResponse(line1 + line2)
