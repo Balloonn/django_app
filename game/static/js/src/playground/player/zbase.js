1c0e839aca79077b931a1c7d3ea6627813b092c6
Balloonn <wangzhongnan@emails.bjut.edu.cn>
2022-08-30 18:06:18 +0800

    add AI attack Particle

--- game/static/js/src/playground/player/zbase.js
+++ game/static/js/src/playground/player/zbase.js
@@ -5,6 +5,9 @@ class Player extends AcGameObject {
         this.y = y;
         this.vx = 0;
         this.vy = 0;
+        this.damage_x = 0;
+        this.damage_y = 0;
+        this.damage_speed = 0;
         this.move_length = 0;
         this.playground = playground;
         this.ctx = this.playground.game_map.ctx;
@@ -13,6 +16,8 @@ class Player extends AcGameObject {
         this.speed = speed;
         this.is_me = is_me;
         this.eps = 0.1;
+        this.friction = 0.9;
+        this.spent_time = 0;
 
         this.cur_skill = null;
     }
@@ -20,8 +25,11 @@ class Player extends AcGameObject {
     start() {
         if(this.is_me) {
             this.add_listening_events();
+        } else {
+            let tx = Math.random() * this.playground.width;
+            let ty = Math.random() * this.playground.height;
+            this.move_to(tx,ty);
         }
-
     }
 
     add_listening_events() {
@@ -57,7 +65,7 @@ class Player extends AcGameObject {
         let color = "orange";
         let speed = this.playground.height * 0.5;
         let move_length = this.playground.height * 1;
-        new FireBall(this.playground, this, x, y, radius, vx, vy, color, speed, move_length);
+        new FireBall(this.playground, this, x, y, radius, vx, vy, color, speed, move_length, this.playground.height * 0.01);
     }
 
     get_dist(x1,y1,x2,y2){
@@ -73,15 +81,59 @@ class Player extends AcGameObject {
         this.vy = Math.sin(angle);
     }
 
+    is_attacked(angle, damage) {
+
+        for(let i = 0; i < 20 + Math.random() * 5; i ++ ) {
+            let x = this.x, y = this.y;
+            let radius = this.radius * Math.random() * 0.1;
+            let angle = Math.PI * 2 * Math.random();
+            let vx = Math.cos(angle), vy = Math.sin(angle);
+            let color = this.color;
+            let speed = this.speed * 10;
+            let move_length = this.radius * Math.random() * 5;
+            new Particle(this.playground, x, y, radius, vx, vy, color, speed, move_length);
+        }
+
+        this.radius -= damage;
+        if(this.radius < 10) {
+            this.destroy();
+            return false;
+        }
+
+        this.damage_x = Math.cos(angle);
+        this.damage_y = Math.sin(angle);
+        this.damage_speed = damage * 100;
+        this.speed *= 1.25;
+
+    }
+
     update() {
-        if(this.move_length < this.eps) {
-            this.move_length = 0;
+        this.spent_time += this.timedelta / 1000;
+        if(this.spent_time > 4 && Math.random() < 1 / 200.0) {
+            let player = this.playground.players[0];
+            if(!this.is_me) this.shoot_fireball(player.x, player.y);
+        }
+        if(this.damage_speed > 10) {
             this.vx = this.vy = 0;
+            this.move_length = 0;
+            this.x += this.damage_x * this.damage_speed * this.timedelta / 1000;
+            this.y += this.damage_y * this.damage_speed * this.timedelta / 1000;
+            this.damage_speed *= this.friction;
         } else {
-            let moved = Math.min(this.move_length, this.speed * this.timedelta / 1000);
-            this.x += this.vx * moved;
-            this.y += this.vy * moved;
-            this.move_length -= moved;
+            if(this.move_length < this.eps) {
+                this.move_length = 0;
+                this.vx = this.vy = 0;
+                if(!this.is_me) {
+                    let tx = Math.random() * this.playground.width;
+                    let ty = Math.random() * this.playground.height;
+                    this.move_to(tx,ty);
+                }
+            } else {
+                let moved = Math.min(this.move_length, this.speed * this.timedelta / 1000);
+                this.x += this.vx * moved;
+                this.y += this.vy * moved;
+                this.move_length -= moved;
+            }
         }
         this.render();
     }
